6c5c70e76f7f13856949fb8885c78304fd9140ac
Djioleu Florian <nsiegningflorian@gmail.com>
2023-05-31 14:41:11 +0100

    Display critic score

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
 import App from "./App.tsx";
 
+import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
